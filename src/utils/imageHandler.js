35328a9c48efc954e222bb35a48610625762e2f8
stevanjulian4-wq <stevanjulian4@gmail.com>
2026-09-11 20:40:34 -0500

    Update imageHandler.js

--- src/utils/imageHandler.js
+++ src/utils/imageHandler.js
@@ -4,27 +4,27 @@ const { AttachmentBuilder } = require('discord.js');
 
 // Function to get the interface image
 function getInterfaceImage() {
-  const imagePath = path.join(__dirname, '../assets/Interface.webp');
+  const imagePath = path.join(__dirname, '../assets/NewInterface.webp');
   
   // Check if the image exists
   if (fs.existsSync(imagePath)) {
-    const attachment = new AttachmentBuilder(imagePath, { name: 'Interface.webp' });
-    attachment.name = 'Interface.webp';
+    const attachment = new AttachmentBuilder(imagePath, { name: 'NewInterface.webp' });
+    attachment.name = 'newInterface.webp';
     return attachment;
   }
   
   // Try alternative image formats if webp doesn't exist
   const pngPath = path.join(__dirname, '../assets/interface.png');
   if (fs.existsSync(pngPath)) {
-    const attachment = new AttachmentBuilder(pngPath, { name: 'interface.png' });
-    attachment.name = 'interface.png';
+    const attachment = new AttachmentBuilder(pngPath, { name: 'NewInterface.png' });
+    attachment.name = 'newinterface.png';
     return attachment;
   }
   
   const jpgPath = path.join(__dirname, '../assets/interface.jpg');
   if (fs.existsSync(jpgPath)) {
-    const attachment = new AttachmentBuilder(jpgPath, { name: 'interface.jpg' });
-    attachment.name = 'interface.jpg';
+    const attachment = new AttachmentBuilder(jpgPath, { name: 'Newinterface.jpg' });
+    attachment.name = 'newinterface.jpg';
     return attachment;
   }
   
